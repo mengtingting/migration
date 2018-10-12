891ff6a8ee9c9a3e1bbbc3a9181ee5fbb66ad60c
mengtingting <857334730@qq.com>
2018-10-12 13:26:04 +0800

    3
    3

--- database/migrations/2018_10_12_051344_create_users_add_deleted_at_table.php
+++ database/migrations/2018_10_12_051344_create_users_add_deleted_at_table.php
@@ -14,7 +14,7 @@ class CreateUsersAddDeletedAtTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint  $table) {
-            $table->dateTime('deleted_at');
+            $table->dateTime('deleted_at')->nullable();
         });
     }
 
