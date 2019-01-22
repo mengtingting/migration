413fb4491e758677494e2b2e3cd21eef5f6aad62
mengtingting <857334730@qq.com>
2019-01-22 16:17:57 +0800

    提交代码
    提交代码

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,14 @@
 */
 
 
-
+Route::get('/admin/login', 'Admin\LoginController@login');        //登录
+Route::post('/admin/login', 'Admin\LoginController@loginPost');   //post登录请求
+Route::get('/admin/loginout', 'Admin\LoginController@loginout');  //注销
 Route::group(['prefix' => 'admin', 'middleware' => ['admin.login']], function () {
 
+//首页
+    Route::get('/', 'Admin\IndexController@index');       //首页
+    Route::get('/index', 'Admin\IndexController@index');  //首页
 
 
 });
